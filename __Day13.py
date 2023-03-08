2b3e8d47185e536099ecd1c672c066fd6e7ccbef
pweemaes-ro <zappamotel@quicknet.nl>
2023-03-08 18:22:15 +0100

    Before refactoring '<'

--- __Day13.py
+++ __Day13.py
@@ -1,141 +1,94 @@
 """Day 13: Distress Signal"""
+from __future__ import annotations
 import time
-from functools import total_ordering
 from itertools import zip_longest
-from math import prod
-from typing import TypeVar
 
-T = TypeVar("T")
 
-def bubble_sort(arr: list[T]):
-    n = len(arr)
+def bubble_sort(packets: list[Packet]) -> None:
+    """Slightly modified bubble sort. "Swap if the element found is greater
+    than the next element" is changed to "Swap if the element found is
+    not smaller than the next element" (reason: Packet implements the '<'
+    operator, not the '>' operator."""
 
-    # Traverse through all array elements
-    for i in range(n):
+    nr_of_packets = len(packets)
+
+    # Traverse through all list elements
+    for i in range(nr_of_packets):
 
         # Last i elements are already in place
-        for j in range(0, n - i - 1):
+        for j in range(nr_of_packets - i - 1):
 
-            # traverse the array from 0 to n-i-1
-            # Swap if the element found is greater
-            # than the next element
-            # if arr[j] > arr[j + 1]:
-            #     arr[j], arr[j + 1] = arr[j + 1], arr[j]
-            if Packet(arr[j]) > Packet(arr[j + 1]):
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+            # traverse the array from 0 to nr_of_packets - i - 1
+            # (Swap if the element found is "greater than" changed to "not
+            # smaller than").
+            if not packets[j] < packets[j + 1]:
+                packets[j], packets[j + 1] = packets[j + 1], packets[j]
 
-@total_ordering
+
 class Packet(list):
-    _verbose: bool = False
+    """Simple class derived from list. It supports the __lt__ operator, so we
+    can write Packet_1 < Packet_2 to compare two Packet instances. This is
+    needed by bubblesort algorithm used for sorting ALL packets in part 2."""
 
     def __init__(self, packet: list):
-        if isinstance(packet, int):
-            packet = [packet]
         super().__init__(packet)
 
-    @property
-    def verbose(self) -> bool:
-        return self._verbose
-
-    @verbose.setter
-    def verbose(self, value) -> None:
-        self._verbose = value
-
-    def __gt__(self, other):
+    def __lt__(self, other):
         done = False
         result = True
-        global compares_count, convergence_count, exhaustion_count, iter_count
-        right = other
 
-        def inner(left, right):
+        def _inner(left, right):
             nonlocal done, result
-            global compares_count, convergence_count, exhaustion_count, \
-                iter_count
 
             if done:
                 return
-
             elif isinstance(left, int) and isinstance(right, int):
-                if self.verbose:
-                    print(f"Comparing ints {left} and {right}")
-                    compares_count += 1
                 if left != right:
-                    if left < right:
-                        if self.verbose:
-                            print(f"  {left} < {right} => SORT OK")
-                    else:
-                        if self.verbose:
-                            print(f"  {left} > {right} => SORT NOT OK")
                     result = left < right
                     done = True
-
             elif isinstance(left, int) and isinstance(right, list):
-                convergence_count += 1
-                if self.verbose:
-                    print(f"Converted left {left} to {[left]} "
-                          f"vs. right {right}")
-                inner([left], right)
-
+                _inner([left], right)
             elif isinstance(left, list) and isinstance(right, int):
-                convergence_count += 1
-                if self.verbose:
-                    print(f"Converted right {right} to {[right]} "
-                          f"vs. left {left}")
-                inner(left, [right])
-
+                _inner(left, [right])
             else:
                 sentinel = object()
                 for i, j in zip_longest(left, right, fillvalue=sentinel):
-                    iter_count += 1
                     if done:
                         return
-
                     if i is sentinel:
-                        exhaustion_count += 1
-                        if self.verbose:
-                            print(f"Left exhausted first => SORT OK")
                         done = True
                         result = True
-
                     elif j is sentinel:
-                        exhaustion_count += 1
-                        if self.verbose:
-                            print(f"Right exhausted first => SORT NOT OK")
                         done = True
                         result = False
-
                     else:
                         if not done:
-                            inner(i, j)
+                            _inner(i, j)
 
-        inner(self, right)
+        _inner(self, other)
 
-        if self.verbose:
-            print(f"{compares_count = }")
-            print(f"{convergence_count = }")
-            print(f"{exhaustion_count = }")
-            print(f"{iter_count = }")
-
-        return not result
+        return result
 
 
 def get_packet(line: str) -> list:
+    """Returns the packet as a nested list of integers and more such (nested)
+    lists. Using eval is (in general) a very bad and risky idea, but it is
+    also very easy in our case! And it's not like this code is ever going to
+    be run in a multinational's production environment... ;-)."""
+
     return eval(line)
 
 
 def get_pairs_of_packets(lines: list[str]) -> list:
+    """Converts all lines to pairs of packets."""
     return [(Packet(get_packet(lines[i * 3])),
              Packet(get_packet(lines[i * 3 + 1])))
             for i in range((len(lines) + 1) // 3)]
 
 
-compares_count = 0
-convergence_count = 0
-exhaustion_count = 0
-iter_count = 0
+def main() -> None:
+    """Solve the puzzle."""
 
-
-def main():
     part_1 = "Determine which pairs of packets are already in the right " \
              "order. What is the sum of the indices of those pairs?"
 
@@ -149,49 +102,27 @@ def main():
         lines = input_file.readlines()
         pairs_of_packets = get_pairs_of_packets([line[:-1] for line in lines])
 
+    solution_1 = 0
 
-    results = []
-    for i in range(len(pairs_of_packets)):
-        left, right = pairs_of_packets[i]
-        sort_ok = (right > left)
-        if not sort_ok:
-            pairs_of_packets[i] = [right, left]
-        results.append((i + 1, sort_ok))
-    solution_1 = sum(result[0] for result in results if result[1])
+    firsts: list[Packet] = list()
+    seconds: list[Packet] = list()
+    for i, (left, right) in enumerate(pairs_of_packets, start=1):
+        if left < right:
+            firsts.append(left)
+            seconds.append(right)
+            solution_1 += i
+        else:
+            firsts.append(right)
+            seconds.append(left)
 
-    firsts_in_pairs = [left for (left, right) in pairs_of_packets]
-    bubble_sort(firsts_in_pairs)
+    solution_2 = 1
 
-    seconds_in_pairs = [right for (left, right) in pairs_of_packets]
-    bubble_sort(seconds_in_pairs)
-
-    packet_two = Packet([[2]])
-    packet_six = Packet([[6]])
-    all_packets = firsts_in_pairs + seconds_in_pairs + [packet_two, packet_six]
-
-
-    bubble_sort(all_packets, initial_offset=1)
-    locations = []
-    for i, packet in enumerate(all_packets):
-        if packet in (packet_two, packet_six):
-            locations.append(i+1)
-    # print("*" * 10)
-    # print(f"{locations}")
-    # print(prod(locations))
-    # results = [left < right for left, right in pairs_of_packets]
-    # indices = [i for i, result in enumerate(results, start=1) if result]
-    # solution_1 = sum(indices)
-
-    solution_2 = prod(locations)
-    # For solution p, make sure you presort the pairs according to the results
-    # of part 1. Create a datastructure (dict()) using tuple(package_1) as key,
-    # and another dict(package_1, compare) where compare = True if package 1
-    # comes before package 2, or False if package_2 comes before package_1.
-    # This implies writing a key function. Problems:
-    # How to avoid double entries like this:
-    # dict[package1] = {pacakge2: True, ...} vs.
-    # dict[package2] = {pacakge1: False, ...}
-    # Both have same information, so it should be stored only once...
+    extra_packets = [Packet([[2]]), Packet([[6]])]
+    all_packets = firsts + seconds + extra_packets
+    bubble_sort(all_packets)
+    for i, packet in enumerate(all_packets, start=1):
+        if packet in extra_packets:
+            solution_2 *= i
 
     stop = time.perf_counter_ns()
 
