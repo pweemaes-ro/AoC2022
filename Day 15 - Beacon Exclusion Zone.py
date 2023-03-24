059a5c5957315e31ca68e1dbd2166ee09a799913
pweemaes-ro <zappamotel@quicknet.nl>
2023-03-24 11:21:44 +0100

    Day 15: Before major overhaul...

--- Day 15 - Beacon Exclusion Zone.py
+++ Day 15 - Beacon Exclusion Zone.py
@@ -1,110 +1,107 @@
 """Day 15: Beacon Exclustion Zone"""
 import re
 import time
-from dataclasses import dataclass
 from typing import Generator
 
 XRange = tuple[int, int]
+Coordinate = tuple[int, int]
 
 
-@dataclass
-class Coordinate:
-    """Simple hashable class to represent a coordinate in a 2d plane."""
-
-    x: int
-    y: int
-
-    def __hash__(self):
-        return hash(repr((self.x, self.y)))
-    #
-    # def __lt__(self, other):
-    #     if isinstance(other, Coordinate):
-    #         return ((self.x, self.y) < (other.x, other.y))
-
-
-@dataclass
 class SBInfo:
     """Represents the sensor beacon data: their locations + distance."""
-
-    sensor: Coordinate
 
     def __init__(self, sensor: Coordinate, beacon: Coordinate):
         self.sensor = sensor
         self.beacon = beacon
-        self.distance = abs(self.beacon.x - self.sensor.x) + \
-            abs(self.beacon.y - self.sensor.y)
+        self.distance = abs(self.beacon[0] - self.sensor[0]) + \
+            abs(self.beacon[1] - self.sensor[1])
 
 
-def _x_ranges_merger(ranges: list[tuple[int, int]]) \
-        -> Generator[list[int, int], None, None]:
-    """Yields x-ranges [p, q] of integers with p <= q. Consecutive ranges that
-    overlap are 'merged' before yielded. Assumes that the ranges are sorted,
-    that is, for every pair of successive ranges [a, b] and [c, d] we have
-    a <= c, and if a == c then also b <= d. Two successive ranges [a, b] and
-    [c, d] will be merged if c <= b + 1 (since we expect ints!), so [1, 5] and
-    [6, 10] will be merged to [1, 10]."""
+def _x_ranges_merger(ranges: list[XRange]) -> Generator[XRange, None, None]:
+    """Yields x-ranges (p, q) of integers with p <= q. Consecutive x-ranges
+    that overlap are 'merged' before yielded. Assumes that the x-ranges are
+    sorted, that is, for every pair of successive x-ranges (a, b) and (c, d)
+    we have a <= c, and if a == c then also b <= d. Two successive x-ranges
+    (a, b) and (c, d) will be merged if c <= b + 1 (since we expect ints!), so
+    (1, 5) and (6, 10) will be merged to (1, 10)."""
 
     p, q = ranges[0]
 
-    for (r, s) in ranges[1:]:
+    for r, s in ranges[1:]:
         if r <= q + 1:
             q = max(q, s)
         else:
-            yield [p, q]
+            yield p, q
             p, q = r, s
-    yield [p, q]
+    yield p, q
 
 
-def _get_sensor_beacon_coordinates(line) -> tuple[Coordinate, ...]:
-    ints = [*map(int, re.findall(r"-*\d+", line))]
-    sensor = Coordinate(ints[0], ints[1])
-    beacon = Coordinate(ints[2], ints[3])
+def _get_sensor_beacon_coordinates(line: str) -> tuple[Coordinate, Coordinate]:
+    """Convert the line to sensor- and beacon coordinates."""
 
-    return sensor, beacon
+    ints: list[int] = [*map(int, re.findall(r"-*\d+", line))]
+    return (ints[0], ints[1]), (ints[2], ints[3])
 
 
-def solve_it(filename: str) -> tuple[int, int]:
-    """Solve the puzzle and return the solutions."""
+def _solve_it(sb_infos: tuple[SBInfo, ...],
+              part_1_row_nr: int,
+              max_rownr: int) -> tuple[int, int]:
 
-    with open(filename) as input_file:
-        lines = input_file.readlines()
+    # Todo: Outer loop over sb_infos, inner loop only over relevant y (those
+    #       within reach of the sensor_beacon distance).
 
-    sb_infos = [SBInfo(*_get_sensor_beacon_coordinates(line))
-                for line in lines]
-
-    solution_1 = solution_2 = None
-
-    for y_coordinate in range(4_000_001):
+    solution_1 = solution_2 = 0
+    for y_coordinate in range(max_rownr + 1):
         x_ranges: list[XRange] = []
 
         for sb_info in sb_infos:
-            sensor_row_distance = abs(sb_info.sensor.y - y_coordinate)
+            sensor_row_distance = abs(sb_info.sensor[1] - y_coordinate)
             radius = sb_info.distance - sensor_row_distance
             if radius >= 0:
-                x_ranges.append((sb_info.sensor.x - radius,
-                                 sb_info.sensor.x + radius))
+                x_ranges.append((sb_info.sensor[0] - radius,
+                                 sb_info.sensor[0] + radius))
 
-        x_ranges = list(_x_ranges_merger(sorted(x_ranges)))
+        if not x_ranges:
+            continue
 
-        if y_coordinate == 2_000_000:
+        merged_ranges = tuple(_x_ranges_merger(sorted(x_ranges)))
+
+        if y_coordinate == part_1_row_nr:
             nr_beacons_on_row = len(set(sb_info.beacon
                                         for sb_info in sb_infos
-                                        if sb_info.beacon.y == y_coordinate))
+                                        if sb_info.beacon[1] == y_coordinate))
             nr_excluded_on_row = sum(x_range[1] - x_range[0] + 1
-                                     for x_range in x_ranges)
+                                     for x_range in merged_ranges)
             solution_1 = nr_excluded_on_row - nr_beacons_on_row
 
             if solution_2:
                 break
 
-        if len(x_ranges) > 1:
-            x_coordinate = x_ranges[0][1] + 1
-            solution_2 = 4_000_000 * x_coordinate + y_coordinate
+        nr_merged_ranges = len(merged_ranges)
+        if nr_merged_ranges > 1:
+            for i in range(nr_merged_ranges):
+                x_coordinate = merged_ranges[i][1] + 1
+                if x_coordinate >= 0:
+                    solution_2 = 4_000_000 * x_coordinate + y_coordinate
+                    break
 
             if solution_1:
                 break
 
     return solution_1, solution_2
+
+
+def solve_it(filename: str, s1_row: int, max_rownr: int) -> tuple[int, int]:
+    """Solve the puzzle and return the solutions."""
+
+    with open(filename) as input_file:
+        lines = input_file.readlines()
+
+    sb_infos: tuple[SBInfo, ...] = \
+        tuple(SBInfo(*_get_sensor_beacon_coordinates(line))
+              for line in lines)
+
+    return _solve_it(sb_infos, s1_row, max_rownr)
 
 
 def main():
@@ -117,16 +114,23 @@ def main():
 
     start = time.perf_counter_ns()
 
-    solution_1, solution_2 = solve_it("input_files/day15.txt")
+    # solution_1, solution_2 = solve_it("input_files/day15t1.txt",
+    #                                   part_1_row_nr=10,
+    #                                   max_rownr=20)
+
+    solution_1, solution_2 = solve_it("input_files/day15.txt",
+                                      s1_row=2_000_000,
+                                      max_rownr=4_000_000)
+
     stop = time.perf_counter_ns()
 
-    assert solution_1 == 4424278
-    print(f"Day 15 part 1: {part_1} {solution_1}")
+    assert solution_1 == 4_424_278
+    print(f"Day 15 part 1: {part_1} {solution_1:_}")
 
-    assert solution_2 == 10382630753392
-    print(f"Day 15 part 2: {part_2} {solution_2}")
+    assert solution_2 == 10_382_630_753_392
+    print(f"Day 15 part 2: {part_2} {solution_2:_}")
 
-    print(f"Day 15 took {(stop - start) * 10 ** -6:.3f} ms")
+    print(f"Day 15 took {(stop - start) * 10 ** -6:_.3f} ms")
 
 
 if __name__ == "__main__":
