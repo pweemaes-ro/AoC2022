149ae8f0010bb01a532c9c169610d4b886e87d1f
pweemaes-ro <zappamotel@quicknet.nl>
2023-04-21 18:16:18 +0200

    Major improvement (thanks to code from Reddit with a compare function for the key in Python's sorted function).

--- Day 13 - Distress Signal.py
+++ Day 13 - Distress Signal.py
@@ -2,89 +2,58 @@
 from __future__ import annotations
 
 import time
-from itertools import zip_longest
-from typing import Any
+from functools import cmp_to_key
+from itertools import chain
+from math import prod
+from typing import cast, TypeAlias
 
-from AoCLib.Sorters import SorterStrategy, MergeSort
+# type aliases
+PacketList: TypeAlias = list[int | list[int]]
 
 
-class Packet(list):
+class Packet(PacketList):
     """Simple class derived from list. It supports the __lt__ operator, so we
     can write Packet_1 < Packet_2 to compare two Packet instances. This is
     used directly in part 1 (if packet_1 < packet_2) and by the sorting
     algorithms in AoCLib.Sorters (i.c. MergeSort) in part 2."""
 
-    # Packet class implements the SupportsGreaterThan protocol (although it
-    # does not need to know that, it is nowhere in the definition of Packet).
-    # Packet is therefore an IMPLICIT subtype of SupportsGreaterThan.
-    # Typecheckers like Mypy will complain if we pass a list of Packet objects
-    # to the sort method of a SorterStrategy implementation, since this method
-    # expects a list[SupportGreaterThan] as argument.
-    def __init__(self, packet: list[int | list]):
+    def __init__(self, packet: PacketList, packet_id: int) -> None:
         super().__init__(packet)
-
-    def __lt__(self, other: Any) -> bool:
-        # Required for min(packet1, packet_2)
-        return (not self > other) and (self != other)
-
-    def __gt__(self, other: Any) -> bool:
-        # Required for max(packet1, packet_2) and SorterStrategy
-        # implementations (can sort objects implementing SupportsGreaterThan
-        # protocol only!).
-
-        # I'm not too thrilled about this compare method. Sure, it's recursive
-        # and therefore quite short, but my gut feeling tells me it can be
-        # done smarter than this... Just couldn't figure it out yet.
-        done = False
-        result = True
-        sentinel = object()
-
-        def _inner(left: Any, right: Any) -> None:
-            nonlocal done, result
-
-            if done:
-                return
-            elif isinstance(left, int) and isinstance(right, int):
-                if left != right:
-                    result = left > right
-                    done = True
-            elif isinstance(left, int) and isinstance(right, list):
-                _inner([left], right)
-            elif isinstance(left, list) and isinstance(right, int):
-                _inner(left, [right])
-            else:
-                for i, j in zip_longest(left, right, fillvalue=sentinel):
-                    if i is sentinel or j is sentinel:
-                        result = j is sentinel
-                        done = True
-                        return
-                    else:
-                        _inner(i, j)
-                        if done:
-                            return
-
-        _inner(self, other)
-
-        return result
+        self.packet_id = packet_id
 
 
-def get_packet(line: str) -> list[list | int]:
-    """Returns the packet as a nested list of integers and more such (nested)
-    lists. Using eval is (in general) a very bad and risky idea, but it is
-    also very easy in our case! And it's not like this code is ever going to
-    be run in a multinational's production environment... ;-)."""
+def compare(left: int | list[int] | Packet, right: int | list[int] | Packet) \
+        -> int:
+    """This is the wonderfull compare that I wasn't able to come up with
+    myself. Got it from https://www.reddit.com/r/adventofcode/comments/zkmyh4/
+    comment/j00qay8/?utm_source=share&utm_medium=web2x&context=3
 
-    return eval(line)
+    My original code used sorting algo's from AoCLib/Sorters (and for that it
+    also implemented __lt__ and __gt__ on the Packet class). Athough using
+    this compare function is much nicer, the performance of my code wasn't
+    that much worse than using this compare function, and in structure quite
+    similar!"""
+
+    match left, right:
+        case int(), int():
+            assert isinstance(left, int) and isinstance(right, int)
+            return (left > right) - (left < right)
+        case int(), list():
+            assert isinstance(left, int) and isinstance(right, list)
+            return compare([left], right)
+        case list(), int():
+            assert isinstance(left, list) and isinstance(right, int)
+            return compare(left, [right])
+        case list(), list():
+            assert isinstance(left, list) and isinstance(right, list)
+            for z in map(compare, left, right):
+                if z:
+                    return z
+            return compare(len(left), len(right))
+    return 0
 
 
-def get_pairs_of_packets(lines: list[str]) -> list[tuple[Packet, Packet]]:
-    """Converts all lines to pairs of packets."""
-    return [(Packet(get_packet(lines[i * 3])),
-             Packet(get_packet(lines[i * 3 + 1])))
-            for i in range((len(lines) + 1) // 3)]
-
-
-def main(sorter_strategy: SorterStrategy = MergeSort()) -> None:
+def main() -> None:
     """Solve the puzzle."""
 
     part_1 = "Determine which pairs of packets are already in the right " \
@@ -96,30 +65,41 @@ def main(sorter_strategy: SorterStrategy = MergeSort()) -> None:
     start = time.perf_counter_ns()
 
     with open("input_files/day13.txt") as input_file:
-        lines = input_file.readlines()
-        pairs_of_packets = get_pairs_of_packets([line[:-1] for line in lines])
+        lines = input_file.read().splitlines()
 
-    solution_1 = 0
+    # Since eval returns Any, Mypy 'strict' will generate error when assigning
+    # the result to a Packet (which expects a PacketList), I use cast to tell
+    # Mypy that it can relax... (cast has a small price, too little to have
+    # any noticable impact on performance).
+    #
+    # Using eval is (in general) a very bad and risky idea, and should NEVER be
+    # used with code from untrusted sources. However, here we have control
+    # over the input file, so it should not be an issue.
+    #
+    # By assigning a unique packet_id to each packet, such that for each
+    # successive pair in the input we have a pair of ids (i, i + 1), we can
+    # avoid comparing pairs of Packets. All we do is sort ALL packets, and
+    # then create a dict ('packet_id_to_idx') of (id, index) key-value pairs
+    # based on the sorted list of packets (note that index is 1-based). Then
+    # we determine per pair whether it was already in the right order by
+    # checking if dict[i] < dict[i + 1].
+    packets_from_input = (Packet(cast(PacketList, eval(line)), packet_id)
+                          for packet_id, line in enumerate(lines)
+                          if line)
+    extra_packets = (Packet([[2]], -1), Packet([[6]], -2))
+    extra_packets_ids = [packet.packet_id for packet in extra_packets]
+    sorted_packets = sorted(chain(packets_from_input, extra_packets),
+                            key=cmp_to_key(compare))
+    packet_id_to_idx = {packet.packet_id: i
+                        for i, packet in enumerate(sorted_packets, start=1)}
+    nr_of_package_pairs = (len(lines) + 1) // 3
 
-    firsts = []
-    seconds = []
-    for i, (left, right) in enumerate(pairs_of_packets, start=1):
-        if left < right:
-            solution_1 += i
-        firsts.append(min(left, right))
-        seconds.append(max(left, right))
+    solution_1 = sum(pair_idx + 1
+                     for pair_idx in range(nr_of_package_pairs)
+                     if packet_id_to_idx[packet_id := pair_idx * 3]
+                     < packet_id_to_idx[packet_id + 1])
 
-    solution_2 = 1
-
-    extra_packets = [Packet([[2]]), Packet([[6]])]
-    all_packets = firsts + seconds + extra_packets
-    # We have a slight advantage from the fact that we've already put the
-    # firsts before the seconds in all_packets.
-    sorter_strategy.sort(all_packets)
-
-    for i, packet in enumerate(all_packets, start=1):
-        if packet in extra_packets:
-            solution_2 *= i
+    solution_2 = prod(packet_id_to_idx[i] for i in extra_packets_ids)
 
     stop = time.perf_counter_ns()
 
@@ -133,14 +113,4 @@ def main(sorter_strategy: SorterStrategy = MergeSort()) -> None:
 
 
 if __name__ == "__main__":
-    # Main takes sorting strategy as optional parameter with default
-    # MergeSort(). The following strategies were tested: BubbleSort,
-    # InsertionSort, QuickSort and MergeSort. BubbleSort was worst.
-    # InsertionSort is approx. 3 times faster than BubbleSort. QuickSort and
-    # MergeSort are approx. 7 times faster than BubbleSort.
-
-    # main(MergeSort())
-    # main(InsertionSort())
-    # main(BubbleSort())
-    # main(QuickSort())
     main()
